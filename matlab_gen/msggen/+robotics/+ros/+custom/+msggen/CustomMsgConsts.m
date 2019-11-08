396bd0e3c8e928bc0ac809eca4821669969c1922
Paul Gesel <pac48@wildcats.unh.edu>
2019-11-08 09:27:37 -0500

    Added camera code for realsense 3

--- matlab_gen/msggen/+robotics/+ros/+custom/+msggen/CustomMsgConsts.m
+++ matlab_gen/msggen/+robotics/+ros/+custom/+msggen/CustomMsgConsts.m
@@ -6,6 +6,19 @@ classdef CustomMsgConsts
     %   Copyright 2014-2019 The MathWorks, Inc.
     
     properties (Constant)
+        dynamic_reconfigure_BoolParameter = 'dynamic_reconfigure/BoolParameter'
+        dynamic_reconfigure_Config = 'dynamic_reconfigure/Config'
+        dynamic_reconfigure_ConfigDescription = 'dynamic_reconfigure/ConfigDescription'
+        dynamic_reconfigure_DoubleParameter = 'dynamic_reconfigure/DoubleParameter'
+        dynamic_reconfigure_Group = 'dynamic_reconfigure/Group'
+        dynamic_reconfigure_GroupState = 'dynamic_reconfigure/GroupState'
+        dynamic_reconfigure_IntParameter = 'dynamic_reconfigure/IntParameter'
+        dynamic_reconfigure_ParamDescription = 'dynamic_reconfigure/ParamDescription'
+        dynamic_reconfigure_Reconfigure = 'dynamic_reconfigure/Reconfigure'
+        dynamic_reconfigure_ReconfigureRequest = 'dynamic_reconfigure/ReconfigureRequest'
+        dynamic_reconfigure_ReconfigureResponse = 'dynamic_reconfigure/ReconfigureResponse'
+        dynamic_reconfigure_SensorLevels = 'dynamic_reconfigure/SensorLevels'
+        dynamic_reconfigure_StrParameter = 'dynamic_reconfigure/StrParameter'
         dynamics_getC = 'dynamics/getC'
         dynamics_getCRequest = 'dynamics/getCRequest'
         dynamics_getCResponse = 'dynamics/getCResponse'
@@ -39,23 +52,35 @@ classdef CustomMsgConsts
             
             persistent msgList
             if isempty(msgList)
-                msgList = cell(16, 1);
-                msgList{1} = 'dynamics/getCRequest';
-                msgList{2} = 'dynamics/getCResponse';
-                msgList{3} = 'dynamics/getGRequest';
-                msgList{4} = 'dynamics/getGResponse';
-                msgList{5} = 'dynamics/getJRequest';
-                msgList{6} = 'dynamics/getJResponse';
-                msgList{7} = 'dynamics/getMRequest';
-                msgList{8} = 'dynamics/getMResponse';
-                msgList{9} = 'dynamics/getStaticTorquesRequest';
-                msgList{10} = 'dynamics/getStaticTorquesResponse';
-                msgList{11} = 'dynamics/getTRequest';
-                msgList{12} = 'dynamics/getTResponse';
-                msgList{13} = 'dynamics/getTrajTorquesRequest';
-                msgList{14} = 'dynamics/getTrajTorquesResponse';
-                msgList{15} = 'dynamics/setTorquesRequest';
-                msgList{16} = 'dynamics/setTorquesResponse';
+                msgList = cell(28, 1);
+                msgList{1} = 'dynamic_reconfigure/BoolParameter';
+                msgList{2} = 'dynamic_reconfigure/Config';
+                msgList{3} = 'dynamic_reconfigure/ConfigDescription';
+                msgList{4} = 'dynamic_reconfigure/DoubleParameter';
+                msgList{5} = 'dynamic_reconfigure/Group';
+                msgList{6} = 'dynamic_reconfigure/GroupState';
+                msgList{7} = 'dynamic_reconfigure/IntParameter';
+                msgList{8} = 'dynamic_reconfigure/ParamDescription';
+                msgList{9} = 'dynamic_reconfigure/ReconfigureRequest';
+                msgList{10} = 'dynamic_reconfigure/ReconfigureResponse';
+                msgList{11} = 'dynamic_reconfigure/SensorLevels';
+                msgList{12} = 'dynamic_reconfigure/StrParameter';
+                msgList{13} = 'dynamics/getCRequest';
+                msgList{14} = 'dynamics/getCResponse';
+                msgList{15} = 'dynamics/getGRequest';
+                msgList{16} = 'dynamics/getGResponse';
+                msgList{17} = 'dynamics/getJRequest';
+                msgList{18} = 'dynamics/getJResponse';
+                msgList{19} = 'dynamics/getMRequest';
+                msgList{20} = 'dynamics/getMResponse';
+                msgList{21} = 'dynamics/getStaticTorquesRequest';
+                msgList{22} = 'dynamics/getStaticTorquesResponse';
+                msgList{23} = 'dynamics/getTRequest';
+                msgList{24} = 'dynamics/getTResponse';
+                msgList{25} = 'dynamics/getTrajTorquesRequest';
+                msgList{26} = 'dynamics/getTrajTorquesResponse';
+                msgList{27} = 'dynamics/setTorquesRequest';
+                msgList{28} = 'dynamics/setTorquesResponse';
             end
             
             messageList = msgList;
@@ -67,15 +92,16 @@ classdef CustomMsgConsts
             
             persistent svcList
             if isempty(svcList)
-                svcList = cell(8, 1);
-                svcList{1} = 'dynamics/getC';
-                svcList{2} = 'dynamics/getG';
-                svcList{3} = 'dynamics/getJ';
-                svcList{4} = 'dynamics/getM';
-                svcList{5} = 'dynamics/getStaticTorques';
-                svcList{6} = 'dynamics/getT';
-                svcList{7} = 'dynamics/getTrajTorques';
-                svcList{8} = 'dynamics/setTorques';
+                svcList = cell(9, 1);
+                svcList{1} = 'dynamic_reconfigure/Reconfigure';
+                svcList{2} = 'dynamics/getC';
+                svcList{3} = 'dynamics/getG';
+                svcList{4} = 'dynamics/getJ';
+                svcList{5} = 'dynamics/getM';
+                svcList{6} = 'dynamics/getStaticTorques';
+                svcList{7} = 'dynamics/getT';
+                svcList{8} = 'dynamics/getTrajTorques';
+                svcList{9} = 'dynamics/setTorques';
             end
             
             % The message list was already sorted, so don't need to sort
