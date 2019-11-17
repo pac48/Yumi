c2bdf30ae9014802127b9d0538c89c1539ee8e6f
Paul Gesel <pac48@wildcats.unh.edu>
2019-11-17 17:15:10 -0500

    fixed camera calibration

--- camera1/scripts/camera_transform.py
+++ camera1/scripts/camera_transform.py
@@ -7,9 +7,13 @@ import sys
 import rospy
 import cv2
 import numpy as np
+import tf
+import tf2_ros
+from tf import transformations
 from std_msgs.msg import String, Int32MultiArray, Float32MultiArray
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import TransformStamped
 np.set_printoptions(threshold=sys.maxsize)
 
 class image_converter:
@@ -26,10 +30,31 @@ class image_converter:
     self.x_vec = []
     self.camera_coords = [0,0,0,0]
     self.robot_coords = [0,0,0,0]
-    self.transformation_matrix = np.zeros((4,4))
+    self.transformation_matrix = np.eye(4)
+    self.transformation_matrix[3,0:3]=0.0
+    self.br = tf.TransformBroadcaster()
 
-  #  def callback_pixpos(self,data):
-#    self.pixpos = data.data
+
+  def getTransformMsg(self):
+    rotation = tf.transformations.quaternion_from_matrix(self.transformation_matrix)
+    rotation/=np.sqrt(rotation[0]**2+rotation[1]**2+rotation[2]**2+rotation[3]**2)
+    translation = self.transformation_matrix[0:3,3]
+    time = rospy.get_rostime()
+    parent = "world"
+    child ="camera_depth_optical_frame" #"camera_depth_optical_frame"
+    msg = TransformStamped()
+    msg.header.frame_id = parent
+    msg.header.stamp = time
+    msg.child_frame_id = child
+    msg.transform.translation.x = translation[0]
+    msg.transform.translation.y = translation[1]
+    msg.transform.translation.z = translation[2]
+    msg.transform.rotation.x = rotation[0]
+    msg.transform.rotation.y = rotation[1]
+    msg.transform.rotation.z = rotation[2]
+    msg.transform.rotation.w = rotation[3]
+    return msg
+
   def callback_camera_coords2(self, msg):
       self.camera_coords2 = list(msg.data)
       self.camera_coords2.append(1.0)
@@ -45,49 +70,65 @@ class image_converter:
       
     except CvBridgeError as e:
       print(e)
-    #print('camera coords', self.camera_coords)
-    val = np.matrix(self.camera_coords2)
-    print(self.transformation_matrix*val.transpose())
+    print('camera coords', self.camera_coords)
+    #val = np.matrix(self.camera_coords2)
+    msg = self.getTransformMsg()
+    self.br.sendTransformMessage(msg)
     if len(self.camera_coords)!=12:
       return
-    P1_cam = np.array([self.camera_coords[0],self.camera_coords[1],self.camera_coords[2],1])
-    P1_rob = np.array([self.robot_coords[0],self.robot_coords[1],self.robot_coords[2],1])
+    P1_cam = np.matrix([self.camera_coords[0],self.camera_coords[1],self.camera_coords[2],1.0])
+    P1_rob = np.matrix([self.robot_coords[0],self.robot_coords[1],self.robot_coords[2]])
 
-    P2_cam = np.array([self.camera_coords[3],self.camera_coords[4],self.camera_coords[5],1])
-    P2_rob = np.array([self.robot_coords[3],self.robot_coords[4],self.robot_coords[5],1])
+    P2_cam = np.matrix([self.camera_coords[3],self.camera_coords[4],self.camera_coords[5],1.0])
+    P2_rob = np.matrix([self.robot_coords[3],self.robot_coords[4],self.robot_coords[5]])
 
-    P3_cam = np.array([self.camera_coords[6],self.camera_coords[7],self.camera_coords[8],1])
-    P3_rob = np.array([self.robot_coords[6],self.robot_coords[7],self.robot_coords[8],1])
+    P3_cam = np.matrix([self.camera_coords[6],self.camera_coords[7],self.camera_coords[8],1.0])
+    P3_rob = np.matrix([self.robot_coords[6],self.robot_coords[7],self.robot_coords[8]])
 
-    P4_cam = np.array([self.camera_coords[9],self.camera_coords[10],self.camera_coords[11],1])
-    P4_rob = np.array([self.robot_coords[9],self.robot_coords[10],self.robot_coords[11],1])
+    P4_cam = np.matrix([self.camera_coords[9],self.camera_coords[10],self.camera_coords[11],1.0])
+    P4_rob = np.matrix([self.robot_coords[9],self.robot_coords[10],self.robot_coords[11]])
 
-    camera_matrix = np.array([[P1_cam[0],P1_cam[1],P1_cam[2],P1_cam[3]],[P2_cam[0],P2_cam[1],P2_cam   [2],P2_cam[3]],[P3_cam[0],P3_cam[1],P3_cam[2],P3_cam[3]],[P4_cam[0],P4_cam[1],P4_cam[2],P4_cam[3]]])
+    #camera_matrix = np.array([[P1_cam[0],P1_cam[1],P1_cam[2],P1_cam[3]],[P2_cam[0],P2_cam[1],P2_cam   [2],P2_cam[3]],[P3_cam[0],P3_cam[1],P3_cam[2],P3_cam[3]],[P4_cam[0],P4_cam[1],P4_cam[2],P4_cam[3]]])
+    camera_matrix = np.zeros((12,12))
+    rob_vector = np.zeros((12,1))
+    camera_matrix[0,0:4] = P1_cam
+    camera_matrix[1,4:8] = P1_cam
+    camera_matrix[2,8:12] = P1_cam
+    rob_vector[0:3] = P1_rob.transpose()
+    camera_matrix[3,0:4] = P2_cam
+    camera_matrix[4,4:8] = P2_cam
+    camera_matrix[5,8:12] = P2_cam
+    rob_vector[3:6] = P2_rob.transpose()
+    camera_matrix[6,0:4] = P3_cam
+    camera_matrix[7,4:8] = P3_cam
+    camera_matrix[8,8:12] = P3_cam
+    rob_vector[6:9] = P3_rob.transpose()
+    camera_matrix[9,0:4] = P4_cam
+    camera_matrix[10,4:8] = P4_cam
+    camera_matrix[11,8:12] = P4_cam
+    rob_vector[9:12] = P4_rob.transpose()
+    #rob_x = np.array([P1_rob[0],P2_rob[0],P3_rob[0],P4_rob[0]])
+    T_values = np.linalg.solve(camera_matrix, rob_vector)
+    T_values=T_values.transpose()
+    #rob_y = np.array([P1_rob[1],P2_rob[1],P3_rob[1],P4_rob[1]])
+    #second_row = np.linalg.solve(camera_matrix, rob_y)
 
-    rob_x = np.array([P1_rob[0],P2_rob[0],P3_rob[0],P4_rob[0]])
-    top_row = np.linalg.solve(camera_matrix, rob_x)
+    #rob_z = np.array([P1_rob[2],P2_rob[2],P3_rob[2],P4_rob[2]])
+    #third_row = np.linalg.solve(camera_matrix, rob_z)
 
-    rob_y = np.array([P1_rob[1],P2_rob[1],P3_rob[1],P4_rob[1]])
-    second_row = np.linalg.solve(camera_matrix, rob_y)
-
-    rob_z = np.array([P1_rob[2],P2_rob[2],P3_rob[2],P4_rob[2]])
-    third_row = np.linalg.solve(camera_matrix, rob_z)
-
-    rob_offset = np.array([P1_rob[3],P2_rob[3],P3_rob[3],P4_rob[3]])
-    fourth_row = np.linalg.solve(camera_matrix, rob_offset)
-
-    self.transformation_matrix = np.stack((top_row,second_row,third_row,fourth_row))
+    #rob_offset = np.array([P1_rob[3],P2_rob[3],P3_rob[3],P4_rob[3]])
+    fourth_row = np.array([0, 0, 0, 1])
+    self.transformation_matrix = np.stack((T_values[0,0:4],T_values[0,4:8],T_values[0,8:12],fourth_row))
     
-    print('transformation_matrix', self.transformation_matrix)
-
-    try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError as e:
-      print(e)
+    #print('transformation_matrix', self.transformation_matrix)
+    #try:
+    #  self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+    #except CvBridgeError as e:
+    #  print(e)
 
 def main(args):
+  rospy.init_node('transform', anonymous=True)
   ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
   try:
     rospy.spin()
   except KeyboardInterrupt:
--- camera1/scripts/depth.py
+++ camera1/scripts/depth.py
@@ -10,6 +10,8 @@ import numpy as np
 from std_msgs.msg import String, Int32MultiArray, Float32MultiArray
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import Point
+from visualization_msgs.msg import Marker
 import matplotlib.pyplot as plt
 np.set_printoptions(threshold=sys.maxsize)
 
@@ -23,6 +25,26 @@ class image_converter:
     self.pixpos_sub = rospy.Subscriber("pixpos",Int32MultiArray,self.callback_pixpos)
     self.pixpos = [0,0]
     self.x_vec = []
+    self.mkr_pub = rospy.Publisher('/rviz_markers', Marker, queue_size = 10)
+
+  def publishRViZMarker(self, point):
+    """ Publishes a LINE marker that can be visualizaed in RViZ.
+    This implementation takes geometry_msg/Point variables for the
+    start_pos and goal_pos"""
+    mkr = Marker()
+    mkr.header.frame_id = "camera_depth_optical_frame"
+    mkr.action = mkr.ADD
+    mkr.type = mkr.LINE_LIST
+    mkr.scale.x = 0.01
+    mkr.points.append(point)
+    point2 = Point(point.x,point.y,point.z+0.01)
+    mkr.points.append(point2)
+
+    mkr.color.a = 1.0
+    mkr.color.r = 0.
+    mkr.color.g = 0.0
+    mkr.color.b = 1.0
+    self.mkr_pub.publish(mkr)
 
   def callback_pixpos(self,data):
     self.pixpos = data.data
@@ -43,24 +65,26 @@ class image_converter:
     depth = cv_image_array[self.pixpos[1],self.pixpos[0]]
     #print('size', np.shape(cv_image_array))
     print('depth',depth)
-    AR_x = 85.2/640
-    AR_y = 58.0/480
+    AR_x = 85.2/640.0
+    AR_y = 58.0/480.0
     #AR_x = 91.2/480
     #AR_y = 65.5/640
-    a_x = (self.pixpos[0]-640/2)*AR_x
-    a_x = a_x*(np.pi/180)
+    a_x = (self.pixpos[0]-640.0/2.0)*AR_x
+    a_x = a_x*(np.pi/180.0)
     x_pos = depth*np.sin(a_x)
 
 
     z_pos = depth*np.cos(a_x)
 
-    a_y = (self.pixpos[1]-480/2)*AR_y
-    a_y = a_y*(np.pi/180)
+    a_y = (self.pixpos[1]-480.0/2.0)*AR_y
+    a_y = a_y*(np.pi/180.0)
     y_pos = depth*np.sin(a_y)
-    camera_coords = [x_pos,y_pos,z_pos]
+    camera_coords = [x_pos/1000.0,y_pos/1000.0,z_pos/1000.0]
     msg = Float32MultiArray()
     msg.data = camera_coords
     self.pub.publish(msg)
+    point = Point(x_pos/1000.0,y_pos/1000.0,z_pos/1000.0)
+    self.publishRViZMarker(point)
     print('x_pos, y_pos, z_pos')
     print(x_pos, y_pos, z_pos)
     cv2.waitKey(3)
